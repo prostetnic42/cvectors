626101fcae6846d50cd964dd5995ca4041e897ec
prostetnic <bongosito@gmail.com>
2026-04-20 07:40:32 +0300

    Added the library

---
+++ vectors.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int count;
+	int capacity;
+} Header;
+
+#define VECTOR_INIT_CAPACITY 256
+#define VECTOR_GROWTH 2
+
+#define vector_len(vector) ((Header *)(vector) - 1)->count
+#define vector_capacity(vector) ((Header *)(vector) - 1)->capacity
+
+#define vector_push(vector, item) { \
+	if (!vector) (vector) = (void *)((Header *)malloc(sizeof(Header) + VECTOR_INIT_CAPACITY * sizeof(item)) + 1); \
+		if ((vector) == NULL) { \
+			perror("vector_push"); \
+			exit(1); \
+		} \
+		(vector)[vector_len(vector)++] = item; \
+		if (vector_capacity(vector) <= vector_len(vector)) {\
+			Header *p = realloc(((Header*)(vector) - 1), sizeof(Header) + (vector_capacity(vector) *= VECTOR_GROWTH) * sizeof(item)); \
+			if (p == NULL) { \
+				perror("vector_push"); \
+				exit(1); \
+			} \
+			(vector) = (void*)(p + 1); \
+		} \
+	}
+	
+#define vector_free(vector) free((Header *)(vector) - 1)
